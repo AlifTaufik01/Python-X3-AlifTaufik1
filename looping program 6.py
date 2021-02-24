7920a9119a5462a35c6e62d2643213fcda8bce2a
AlifTaufik01 <78463353+AlifTaufik01@users.noreply.github.com>
2021-02-24 13:30:00 +0700

    Add files via upload
    Looping Tugas Alif /1/X3

---
+++ looping program 6.py
@@ -0,0 +1,10 @@
+kata = "nama saya adalah sopan setiawan"
+
+jum=0
+for letter in kata:
+    if letter == "s":
+        jum += 1
+        #continue
+    #print("huruf sekarang: ",letter)
+
+print ("jumlah :",jum)
